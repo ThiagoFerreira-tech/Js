7d4489c93dfe615a4935e90591792bd94153a632
Thiago Ferreira <THIAGO.PROENCA96@GMAIL.COM>
2025-06-02 18:38:12 -0300

    Update Exercicio1.js

--- Fundamentos TI/controle/ExerciciosCode/Exercicio1.js
+++ Fundamentos TI/controle/ExerciciosCode/Exercicio1.js
@@ -220,4 +220,313 @@ function eDivisivelPor3(numero) {
     return numero % 3 === 0;    
 }
 
-console.log(eDivisivelPor3(150
+console.log(eDivisivelPor3(150)
+
+
+/* 11)  As regras para o cálculo dos anos bissextos são as seguintes: 
+De 4 em 4 anos é ano bissexto; 
+De 100 em 100 anos não é ano bissexto; 
+De 400 em 400 anos é ano bissexto; 
+Prevalecem as últimas regras sobre as primeiras. 
+Partindo daí elabore uma função que recebe um ano e calcula se ele é ano bissexto, imprimindo no console a 
+mensagem e retornando  true ou false.  */
+
+function calcularAnoBissexto (ano) {
+    if (ano <= 0) {
+        return false
+    } else if (ano % 400 == 0) {
+        return true 
+    } else if (ano % 100 == 0) {
+        return false  
+    } else if (ano % 4 == 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(calcularAnoBissexto(0))
+console.log(calcularAnoBissexto(4))
+console.log(calcularAnoBissexto(2022))
+console.log(calcularAnoBissexto(205))
+console.log(calcularAnoBissexto(2404))
+console.log(calcularAnoBissexto(2104))
+
+
+/* 13)Crie um programa que exibe se um dia é dia útil, fim de semana ou dia inválido dado o número referente ao 
+dia. Considere que domingo é o dia 1 e sábado é o dia 7. Utilize a estrutura Switch.  */
+
+function diasDaSemana(dias) {
+    switch (dias)  {
+        case 1:
+        case 7:
+        console.log('Fim de Semana')
+            break
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        console.log('Dia Útil')
+            break
+            default:
+        console.log('Dia Inválido');
+    }
+}
+
+diasDaSemana(2)
+diasDaSemana(8)
+diasDaSemana(7)
+diasDaSemana(1)
+diasDaSemana(5)
+
+
+/* 14) Crie uma estrutura condicional switch que receba uma string com o nome de uma fruta e que possua três 
+casos: Caso maçã, retorne no console: “Não vendemos esta fruta aqui”. Caso kiwi, retorne: “Estamos com 
+escassez de kiwis”. Caso melancia, retorne: “Aqui está, são 3 reais o quilo”. Teste com estas três opções .Crie 
+também um default, que retornará uma mensagem de erro no console.  */
+
+
+function venderFrutas(frutas) {
+    switch (frutas)  {
+        case 'Maça':
+        console.log('Não Vendemos esta fruta aqui.')
+        break
+        case 'Kiwi':
+        console.log('Estamos com escassez de kiwis')
+        break
+        case 'Melancia':
+        console.log('Aqui esta, são 3 reais o quilo')
+        break
+        default :
+        console.log('Não Temos Esta Fruta Para Venda, Sentimos Muito')
+        break 
+        case 'Alcatra':
+        console.log('Isso nao é fruta')
+        break
+       
+    }
+    
+}
+
+venderFrutas('Maça')
+venderFrutas('Kiwi')
+venderFrutas('Melancia')
+venderFrutas('Uva')
+venderFrutas('Mamão')
+venderFrutas('Alcatra')
+
+
+/* 15) Um homem decidiu ir à uma revenda comprar um carro. Ele deseja comprar um carro hatch, e a revenda 
+possui, além de carros hatch, sedans, motocicletas e caminhonetes. Utilizando uma estrutura switch, caso o 
+comprador queira o hatch, retorne: “Compra efetuada com sucesso”. Nas outras opções, retorne: “Tem certeza 
+que não prefere este modelo?”. Caso seja especificado um modelo que não está disponível, retorne no console: 
+“Não trabalhamos com este tipo de automóvel aqui”. */
+
+function escolhaCarro (modelo) {
+    switch (modelo) {
+        case 'Hatch':
+            console.log('Compra efetuada com sucesso')
+        break
+        case 'Sedans':
+            console.log('Tem certeza que não prefere esrte modelo?')
+        break
+        case 'Motocicletas':
+            console.log('Tem certeza que não prefere este modelo?')
+        break
+        case 'Caminhonetes':
+            console.log('Tem certeza que não prefere este modelo?')
+        break
+        default:
+        console.log('Não trabalhamos com este tipo de automovel aqui')
+        
+    
+
+    }
+}
+
+escolhaCarro('Hatch')
+escolhaCarro('Sedans')
+escolhaCarro('Motocicletas')
+escolhaCarro('Caminhonetes')
+escolhaCarro('Barcos')
+escolhaCarro('Bicicleta')
+escolhaCarro('Caminhão')
+
+
+/* 16) Utilizando a estrutura do Switch faça um programa que simule uma calculadora básicaO programa recebe 
+como parâmetros dois valores numéricos e uma string referente à operação e a realize com os valores 
+numéricos na ordem que foram inseridos. Por exemplo: calculadora (2, ‘+’, 3). A função efetuará a soma de 2 e 
+3. Dica: Os sinais das operações são: ‘+’. ‘-’, ‘*’ e ‘/’.  Crie um caso default para operações inválidas. */
+
+function calculadora (valor1, operacao, valor2)  {
+    switch  (operacao) {
+    case '+':
+        return valor1 + valor2
+    case '-':
+        return valor1 - valor2
+    case '*':
+        return valor1 * valor2
+    case '/':
+        return valor1 / valor2
+    default:
+        return 'Operação Inválida'
+    }
+}
+
+console.log(calculadora(2, '+', 3))
+console.log(calculadora(3, '*', 3))
+console.log(calculadora(100, '-', 50 ))
+console.log(calculadora(50, '/', 100))
+console.log(calculadora(4, 'x', 4))
+
+
+/* 17) Um funcionário irá receber um aumento de acordo com o seu plano de 
+trabalho, de acordo com a tabela abaixo:  
+Plano Aumento 
+A 10% 
+B 15% 
+C 20% 
+Faça uma função que leia o plano de trabalho e o salário atual de um funcionário e calcula e imprime o seu 
+novo salário. Use a estrutura switch e faça um caso default que indique que o plano é inválido. */
+
+function calcularReajuste(plano, salarioAtual) {
+    switch (plano) {
+        case 'A' :
+            return salarioAtual * 1.1
+        case 'B':
+            return salarioAtual * 1.5
+        case 'C': 
+            return salarioAtual * 2.0
+        default:
+            return 'Plano inválido'
+
+    }
+ }
+ 
+console.log(calcularReajuste('A', 1000))
+console.log(calcularReajuste('B', 1000))
+console.log(calcularReajuste('C', 1000))
+console.log(calcularReajuste('D', 1000))
+console.log(calcularReajuste('E', 1000))
+
+/* 18) Faça um programa que leia um número entre 0 e 10, e escreva este número por extenso. Use o comando 
+switch. Crie um case default que escreva ‘Número fora do intervalo.’  */
+
+function leitor(numero) {
+    switch (numero) {
+    case 0:
+    console.log('Zero')    
+        break
+    case  1:
+    console.log('Um')
+        break
+    case 2:
+    console.log('Dois')
+        break
+    case 3:
+    console.log('Três')
+        break
+    case 4:
+     console.log('Quatro')
+        break
+    case 5:
+    console.log('Cinco')
+        break
+    case 6:
+    console.log('Seis')
+    break
+    case 7:
+    console.log('Sete')
+        break
+    case 8:
+    console.log('Oito')
+        break
+    case 9: 
+    console.log('Nove')
+        break
+    case 10:
+    console.log('Dez')
+default:
+    console.log('Numero Inválido')
+
+    }
+
+}
+leitor(0)
+leitor(1)
+leitor(2)
+leitor(3)
+leitor(4)
+leitor(5)
+leitor(6)
+leitor(7)
+leitor(8)
+leitor(9)
+leitor(10)
+leitor(11)
+leitor(12)
+
+
+/* 19) O cardápio de uma lanchonete é o seguinte: 
+Código 
+Descrição do Produto 
+Preço 
+100 
+Cachorro Quente 
+R$ 3,00 
+200 
+Hambúrguer Simples 
+R$ 4,00 
+300 
+Cheeseburguer 
+R$ 5,50 
+400 
+Bauru 
+R$ 7,50 
+500 
+Refrigerante 
+R$ 3,50 
+600 
+Suco 
+R$ 2,80 
+Implemente uma  função que receba como parâmetros o código do item pedido, a quantidade e calcule o valor 
+a ser pago por aquele lanche. Considere que a cada execução somente será calculado um item. Use o 
+comando switch. Crie um caso default para produto não existente. */
+
+function parametro (produto, quantidade) {
+    switch (produto) {
+        case 100:
+        return quantidade * 3
+        break
+        case 200:
+        return quantidade * 4
+        break
+        case 300:
+        return quantidade * 5.5
+        break
+        case 400:
+        return quantidade * 7.5
+        break
+        case 500:
+        return quantidade * 3.5
+        break
+        case 600:
+        return quantidade * 2.8
+        break
+        default: 
+        return 'Produto não existente'
+
+        
+    }
+}
+
+console.log(parametro(100, 2))
+console.log(parametro(200, 2))
+console.log(parametro(300, 2))
+console.log(parametro(400, 2))
+console.log(parametro(500, 2))
+console.log(parametro(600, 2))
+console.log(parametro(700, 2))
+
+
